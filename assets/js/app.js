ca50b245a7c867fdb00490dd71b77cd458feb822
Chintan Kotadia <kotadiachintan@gmail.com>
2016-07-27 19:18:44 +0530

    Home page implemented as per sketch

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,14 +4,20 @@
             $('.site-header.navbar-fixed-top').addClass('scroll-active');
         }
 
-        console.log('on scroll');
-
         $(window).on('scroll', function() {
             if($(window).scrollTop() > 30) {
                 $('.site-header.navbar-fixed-top').addClass('scroll-active');
             } else {
                 $('.site-header.navbar-fixed-top').removeClass('scroll-active');
             }
+        });
+
+        $(document).on('click', '.text-highlighted', function() {
+            var data_text = $(this).attr('data-text');
+            $('.text-highlighted').removeClass('text-selected');
+            $(this).addClass('text-selected');
+
+            $('.show-highlighted-text').html(data_text);
         });
     })
 })(jQuery);
